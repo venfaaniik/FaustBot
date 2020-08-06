32eeb52f06d695e730bca84d63519e836cde6079
Kia Mäkinen <kiamakinen@kamk.fi>
2020-08-06 21:50:25 +0300

    sudoku

--- kakkarobotti.py
+++ kakkarobotti.py
@@ -56,15 +56,20 @@ async def on_message(msg):
         if msg.author.id == 227205817642909698:
             await msg.channel.send("Hey there, pretty boy")
         elif msg.author.id == 250389319884210196:
-            await msg.channel.send("Well, how are you going to torture me today?")
+            await msg.channel.send("You cannot kill me in a way that matters.")
         elif msg.author.id == 261970400328548363:
-            await msg.channel.send("Greetings, my Queen")
+            await msg.channel.send("Greetings, me Rhena")
         elif msg.author.id == 314337571377512448:
-            await msg.channel.send("You're so tiny I want to bonk you")
+            await msg.channel.send("You're so tiny I want to pick you up")
         elif msg.author.id == 417072424320761876:
-            await msg.channel.send("<:faust_dread:731300962916106272>")
+            await msg.channel.send("Whenever I see you, I remember that your God really *does* have a sense of humour.")
         else:
-            await msg.channel.send('Hello!')
+            possible_responses = [
+                "Hi.",
+                "Hello.",
+                "Greetings. We done here? Good. Now leave me be."
+            ]
+            await msg.channel.send(random.choice(possible_responses))
 
     if content == ('bye faust'):
         if msg.author.id == 227205817642909698:
@@ -72,60 +77,111 @@ async def on_message(msg):
         elif msg.author.id == 250389319884210196:
             await msg.channel.send("Give me better rolls")
         elif msg.author.id == 261970400328548363:
-            await msg.channel.send("Have a nice day")
+            await msg.channel.send("Me Rhena.")
         elif msg.author.id == 314337571377512448:
             await msg.channel.send("Noot you later")
         elif msg.author.id == 417072424320761876:
-            await msg.channel.send("Turn around and I'll burn your ass, have a nice day otherwise")
+            await msg.channel.send("Turn around and I'll burn your ass, have a nice day though")
         else:
-            await msg.channel.send("See you later!")
+            possible_responses = [
+                "Bye",
+                "Dismissed",
+                "Hmh"
+            ]
+            await msg.channel.send(random.choice(possible_responses))
     
-    if "eternal fire" in content:
+    if any([keyword in content for keyword in ('eternal fire', "witch hunter", "witch hunters")]):
         possible_responses = [
             "<:faust_dread:731300962916106272>",
-            'no.',
-            'keep that away from me.',
-            'yeah, fuck off',
-            'stay back!',
+            'No.',
+            'Keep that talk away from me.',
+            'Yeah, fuck off',
+            'You have the audacity to talk about that bullshit here, in front of me',
+            "How much value do you put on your tongue?",
             'ᵣₑₑₑₑₑₑₑₑₑₑₑ'
-        ]
-        
+        ] 
         await msg.add_reaction(faustDread)
         await msg.channel.send(random.choice(possible_responses))
 
     if content == ("faust?"):
         possible_responses  = [
             "<:faust_dread:731300962916106272>",
-            "yea {}?".format(msg.author.display_name),
-            "what is it, {}?".format(msg.author.display_name)
+            "Yes {}?".format(msg.author.display_name),
+            "What is it, {}?".format(msg.author.display_name)
         ]
         await msg.channel.send(random.choice(possible_responses))
 
     if "wagon" in content:
         possible_responses = [
-            "let's not talk about that",
-            "change the subject, please",
-            "dON'T",
+            "Let's not talk about that.",
+            "Change the subject, please.",
+            "DON'T.",
             "I am pISSED",
-            "now you stay quiet!"
+            "Stay quiet!",
+            "Don't remind me."
+        ]
+        await msg.channel.send(random.choice(possible_responses))
+
+    if any([keyword in content for keyword in ('wagen', 'vagen', 'vvagen', 'vvagon')]):
+        possible_responses = [
+            "Can't even spell it correctly?",
+            "Here, let me help you: Wagon.",
+            "Really? At least you tried."
         ]
         await msg.channel.send(random.choice(possible_responses))
 
     if ("sauna") in content:
-        possible_responses = [
-            "<:eyess:633797028882677760>",
-            "sauna? where?",
-        ]
+        if ("swedish") in content:
+            possible_responses = [
+                "Those can burn.",
+                "Not even a real sauna.",
+                "Those poor souls."
+            ]
+        elif ("wank") in content:
+            possible_responses = [
+                "Really? Can you not?",
+                "You're not worth the dirt to bury you in.",
+                "I am sure even the Church of Eternal Fire disapproves it."
+            ]
+        else:
+            possible_responses = [
+                "<:eyess:633797028882677760>",
+                "Sauna? Where?",
+                "Oh, to find one...",
+                "I miss them."
+            ]
         await msg.channel.send(random.choice(possible_responses))
 
-    if content == ("heart"):
-        await msg.channel.send(content)
+    if ("tent") in content:
+        percent = random.randint(0, 100)
+        if percent == 0:
+            possible_responses = [
+                "I would love to insult you but I'm afraid I won't do as well as nature did.",
+                "Do you get invited to many parties?",
+                "You're not worth the dirt to bury you in.",
+                "You're not worth the energy it'd take to disintegrate you with a simple spell.",
+            ]
+            await msg.channel.send(random.choice(possible_responses))
+        else:
+            possible_responses = [
+                "I don't know what you're talking about",
+                "I am looking away",
+                "I do not see it",
+                "It was the wine",
+                "It was the wind, I swear",
+                "It was the other guy over there",
+                "Tent? What tent?",
+                "Tent? Haven't seen any of those here"
+            ]
+            await msg.channel.send(random.choice(possible_responses))
 
+    if content == ("❤️"):
+        await msg.channel.send("❤️")
 
-    if content.startswith("image"):
+    if content.startswith("bonk"):
         await msg.channel.send("", file=discord.File("fucker.gif"))
 
-    #without this the commands wont work. at all. no touching
+    #without this the commands wont. work. at all. no touching
     await bot.process_commands(msg)
 
 @bot.event
@@ -133,7 +189,7 @@ async def on_member_join(member):
     print("A new member " + member.name + " joined!")
     channel = member.guild.system_channel
     if channel is not None:
-        await channel.send("Welcome {0.mention}! Have a nice stay!".format(member))   
+        await channel.send("Welcome {0.mention}. I hope you enjoy your stay".format(member))   
 
 @bot.command()
 async def DM(ctx, arg, *, message):
@@ -141,6 +197,8 @@ async def DM(ctx, arg, *, message):
     user = await bot.fetch_user(user_id)
     if ctx.author.id == 128440902468370432:
         await user.send(message)
+    elif ctx.author.id == 250389319884210196:
+        await ctx.send("How far are you willing to go to get this power of mine?")
     else:
         await ctx.send("Sorry sweetheart, can't do it for you")
 
@@ -148,6 +206,8 @@ async def DM(ctx, arg, *, message):
 async def DMuser(ctx, user: discord.Member, *, message):
     if ctx.author.id == 128440902468370432:
         await user.send(message)
+    elif ctx.author.id == 250389319884210196:
+        await ctx.send("There is only one being with this kind of power and it is not you.")
     else:
         await ctx.send("Sorry sweetheart, can't do it for you")
 
@@ -158,6 +218,8 @@ async def sendTo(ctx, arg, *, message):
         arg = int(arg)
         channel = bot.get_channel(arg)
         await channel.send(message)
+    elif ctx.author.id == 250389319884210196:
+        await ctx.send("This is something that not even Gods should meddle with.")
     else:
         await ctx.send("Sorry sweetheart, can't do it for you")
 
@@ -167,7 +229,7 @@ async def join(ctx):
     vc = await voicechannel.connect()
     vc.play(discord.FFmpegPCMAudio("testing.mp3"), after=lambda e: print('done', e))
 
-
+#do it like that you can requets for others' playlists too
 @bot.command()
 async def playlist(ctx, arg):
     if arg=="youtube":
@@ -202,7 +264,8 @@ async def roll(ctx, dice: str):
         temp = random.randint(1, limit)
         rollsList.append(temp)
         resultTotal += temp
-    await ctx.send("Your roll: (" + str(resultTotal) + ") \n" + str(rollsList))
+    rollsList = bolding(rollsList, limit)
+    await ctx.send("Your roll: (" + str(resultTotal) + ") \n (" + str(rollsList).replace("'", "").strip("[]") + ")")
 
 #higher
 @bot.command()
@@ -225,7 +288,8 @@ async def roII(ctx, dice: str):
             temp = random.randint(1, limit)
         rollsList.append(temp)
         resultTotal += temp
-    await ctx.send("Your roll: (" + str(resultTotal) + ") \n" + str(rollsList))
+    rollsList = bolding(rollsList, limit)
+    await ctx.send("Your roll: (" + str(resultTotal) + ") \n (" + str(rollsList).replace("'", "").strip("[]") + ")")
 
 #lower
 @bot.command()
@@ -247,9 +311,9 @@ async def rolI(ctx, dice: str):
             temp = random.randint(1, limit)
         rollsList.append(temp)
         resultTotal += temp
-    await ctx.send("Your roll: (" + str(resultTotal) + ") \n" + str(rollsList))
 
-#def bolding(arg):
+    rollsList = bolding(rollsList, limit)
+    await ctx.send("Your roll: (" + str(resultTotal) + ") \n (" + str(rollsList).replace("'", "").strip("[]") + ")")
 
 @bot.command()
 async def w(ctx):
@@ -260,7 +324,21 @@ async def w(ctx):
     unaSaves = random.randint(1, resultTotal)
     totalDead = (resultTotal - unaSaves)
     await ctx.send("Dropped wagons: " + str(wagons) + "\nCollateral damage: (" + str(resultTotal) + ") \nUna manages to save: (" + str(unaSaves) + "). Good job! \nTotal dead: (" + str(totalDead) + "). F to them.")
-    
+
+def bolding(rollList, limit):
+    tempList = []
+    i = 0
+    for x in rollList:
+        tempNmb = rollList[i]
+        if tempNmb == limit or tempNmb == 1:
+            bolded = ("**" + str(tempNmb) + "**")
+            tempList.append(bolded)
+            #print(bolded)
+        else:
+            tempList.append(rollList[i])
+        i += 1
+    #print(tempList)
+    return tempList
 
 # FAUST THE BAAAARD?
 
@@ -278,7 +356,7 @@ async def background_loop():
     #wagons = 11
     while True:
         wagons += random.randint(1,10)
-        print("wagons: " + str(wagons))
+        #print("wagons: " + str(wagons))
         #channel = bot.get_channel("632372863495700519") 
         randMsg = random.randint(1, 8)
         if randMsg == 1:
